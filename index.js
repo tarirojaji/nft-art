b05dbbf0cc5b913377d8e52c27395bc0a3903a88
tarirojaji <57304962+tarirojaji@users.noreply.github.com>
2021-09-07 22:53:44 +0100

    changes

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const { createCanvas, loadImage } = require('canvas');
 const canvas = createCanvas(1000, 1000);
 const ctx = canvas.getContext('2d');
 const { layers, width, height } = require('./input/config.js');
-const edition = 1;
+const edition = 3;
 
 // saves a new image to the directory
-const saveLayer = (_canvas) => {
-    fs.writeFileSync('./newImage.png', _canvas.toBuffer('image/png'));
+const saveLayer = (_canvas, _edition) => {
+    fs.writeFileSync(`./output/${_edition}.png`, _canvas.toBuffer('image/png'));
     console.log('img created');
 
 };
@@ -17,7 +17,7 @@ const drawLayer = async (_layer, _edition) => {
     const image = await loadImage(`${_layer.location}${element.fileName}`);
     ctx.drawImage(image, _layer.position.x, _layer.position.y, _layer.size.width, _layer.size.height);
     console.log(`Created the ${_layer.name} layer with this element ${element.name}`);
-    // saveLayer(canvas);
+    saveLayer(canvas, _edition);
 };
 
 for (let i = 1; i <= edition; i++) {
